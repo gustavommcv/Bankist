b91a20a523fd30b6f941a77d2ab8715d39a987b6
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2024-09-23 17:37:52 -0300

    Flat & FlatMap

--- scripts/script.js
+++ scripts/script.js
@@ -292,3 +292,14 @@ const anyDeposits = movements.some(deposit);
 // EVERY
 // console.log(account4.movements.every(deposit));
 
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+// console.log(arr.flat());
+
+const accountMovements = accounts.map(acc => acc.movements).flat();
+// console.log(accountMovements);
+
+const overalBalance = accountMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
+
+const overalBalance2 = accounts.flatMap(acc => acc.movements).reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance2);
